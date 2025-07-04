d529855734e0d5e8c067adcdb049680c29588aa2
Lajja123 <lajja.lamprostech@gmail.com>
2025-07-04 19:01:41 +0530

    Add: metatag of create job and dashboard page

--- src/app/api/page.tsx
+++ src/app/api/page.tsx
@@ -2,6 +2,32 @@ import React, { Suspense } from "react";
 import ApiPageSkeleton from "@/components/skeleton/ApiPageSkeleton";
 import ApiClientPage from "@/components/api/ApiClientPage";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Generate API  | TriggerX",
+  description: "TriggerX - Web3 Automation Platform.",
+  openGraph: {
+    title: "Generate API  | TriggerX ",
+    description: "TriggerX - Web3 Automation Platform.",
+    url: "https://triggerx-app-nextjs.vercel.app/api",
+    siteName: "TriggerX",
+    images: [
+      {
+        url: "https://triggerx-app-nextjs.vercel.app/OGImages/build.png",
+        width: 1200,
+        height: 630,
+        alt: "Generate API | TriggerX",
+        type: "image/png",
+      },
+    ],
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://triggerx-app-nextjs.vercel.app/api",
+  },
+};
+
 export default function ApiPage() {
   return (
     <Suspense fallback={<ApiPageSkeleton />}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,32 @@ import { CreateJobLayout } from "@/components/create-job/CreateJobLayout";
 import { JobFormProvider } from "@/contexts/JobFormContext";
 import CreateJobSkeleton from "@/components/skeleton/CreateJobSkeleton";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create a Job | TriggerX",
+  description: "TriggerX - Web3 Automation Platform.",
+  openGraph: {
+    title: "Create a Job  | TriggerX ",
+    description: "TriggerX - Web3 Automation Platform.",
+    url: "https://triggerx-app-nextjs.vercel.app/",
+    siteName: "TriggerX",
+    images: [
+      {
+        url: "https://triggerx-app-nextjs.vercel.app/OGImages/build.png",
+        width: 1200,
+        height: 630,
+        alt: "Create a Job | TriggerX",
+        type: "image/png",
+      },
+    ],
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://triggerx-app-nextjs.vercel.app/",
+  },
+};
+
 export default function Home() {
   return (
     <Suspense fallback={<CreateJobSkeleton />}>
