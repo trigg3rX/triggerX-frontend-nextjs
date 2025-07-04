d712951db1b3fcd0e954af2919895e752743a06a
Lajja123 <lajja.lamprostech@gmail.com>
2025-07-04 16:11:29 +0530

    update: name of OGImages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,31 @@ import { QuickActions } from "@/components/dashboard/QuickActions";
 import Statistics from "@/components/dashboard/Statistics";
 import ActiveJobs from "@/components/dashboard/ActiveJobs";
 import DashboardSkeleton from "@/components/skeleton/DashboardSkeleton";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard | TriggerX",
+  description: "TriggerX - Web3 Automation Platform.",
+  openGraph: {
+    title: "Dashboard | TriggerX ",
+    description: "TriggerX - Web3 Automation Platform.",
+    url: "https://triggerx-app-nextjs.vercel.app/dashboard",
+    siteName: "TriggerX",
+    images: [
+      {
+        url: "https://triggerx-app-nextjs.vercel.app/OGImages/dashboard.png",
+        width: 1200,
+        height: 630,
+        alt: "Dashboard | TriggerX",
+        type: "image/png",
+      },
+    ],
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://triggerx-app-nextjs.vercel.app/dashboard",
+  },
+};
 
 function Dashboard() {
   return (
