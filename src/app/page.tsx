fefb62d9ec1a60df60aeec447bbd70f40cc4d7ea
isha-bagadiya <ishabagadiya@lampros.tech>
2025-07-05 12:04:02 +0530

    error handling in dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,32 +5,6 @@ import CreateJobSkeleton from "@/components/skeleton/CreateJobSkeleton";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "TriggerX",
-  description: "TriggerX - Web3 Automation Platform.",
-  openGraph: {
-    title: "TriggerX ",
-    description: "TriggerX - Web3 Automation Platform.",
-    url: "https://triggerx-app-nextjs.vercel.app/",
-    siteName: "TriggerX",
-    images: [
-      {
-        url: "https://triggerx-app-nextjs.vercel.app/OGImages/trigger-og.png",
-        width: 1200,
-        height: 630,
-        alt: "TriggerX",
-        type: "image/png",
-      },
-    ],
-    type: "website",
-  },
-  alternates: {
-    canonical: "https://triggerx-app-nextjs.vercel.app/",
-  },
-};
-
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
   title: "Create a Job | TriggerX",
   description: "TriggerX - Web3 Automation Platform.",
   openGraph: {
