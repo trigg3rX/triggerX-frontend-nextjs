023d3a6df955487013c4e63c85cbd7dce4db5af9
isha-bagadiya <ishabagadiya@lampros.tech>
2025-07-15 20:15:36 +0530

    updated:claim modal code

--- src/components/create-job/templates/components/ClaimModal.tsx
+++ src/components/create-job/templates/components/ClaimModal.tsx
@@ -197,7 +197,7 @@ const ClaimModal: React.FC<ClaimModalProps> = ({ isOpen, onClose }) => {
                   <Typography variant="h3">
                     <ShortAddress address={address} />
                   </Typography>
-                  <LucideCopyButton text={address} />
+                  <LucideCopyButton text={address ?? ""} />
                 </div>
               </div>
               <div className="flex items-center gap-2">
