dea1e2eab05a21851729496cb64cfd8611a96333
Lajja123 <lajja.lamprostech@gmail.com>
2025-08-11 14:12:27 +0530

    Update: fetch timeInterval from typeSpecificData

--- src/hooks/useJobs.ts
+++ src/hooks/useJobs.ts
@@ -142,7 +142,7 @@ export function useJobs() {
                 taskDefinitionId: mapJobType(
                   nextJob.job_data.task_definition_id,
                 ),
-                is_active: nextJob.job_data.is_active === true,
+                is_active: typeSpecificData.is_active === true,
                 job_cost_actual: nextJob.job_data.job_cost_actual,
                 timeFrame: nextJob.job_data.time_frame || "",
                 argType:
@@ -224,7 +224,7 @@ export function useJobs() {
               taskDefinitionId: mapJobType(
                 jobDetail.job_data.task_definition_id,
               ),
-              is_active: jobDetail.job_data.is_active === true,
+              is_active: typeSpecificData.is_active === true,
               linkedJobs: linkedJobsMap[jobDetail.job_data.job_id] || [],
               job_cost_actual: jobDetail.job_data.job_cost_actual,
               timeFrame: jobDetail.job_data.time_frame || "",
