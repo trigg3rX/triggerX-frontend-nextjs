79874a3c9e02984648be7507cf87a982e9c513c6
Lajja123 <lajja.lamprostech@gmail.com>
2025-10-16 11:53:20 +0530

    Update the API service content and added SDK link

--- src/components/api/Documentation.tsx
+++ src/components/api/Documentation.tsx
@@ -17,8 +17,22 @@ function Documentation() {
           align="left"
           className="py-4"
         >
-          Explore and integrate with our Concentration Power Index (CPI)
-          calculation APIs.
+          Explore and integrate your project with TriggerX using API service.
+        </Typography>
+        <Typography variant="h4" color="secondary" align="left" className="">
+          {" "}
+          Accelerate your development with our official SDKs — built for
+          seamless integration in your favorite language. Visit the SDK docs to
+          explore code samples, setup guides, and download links.
+          <br />
+          <a
+            href="https://www.npmjs.com/package/sdk-triggerx"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-500 hover:text-[#F8FF7C] underline"
+          >
+            Check out our SDK
+          </a>
         </Typography>
 
         <ApiDocsPage />
