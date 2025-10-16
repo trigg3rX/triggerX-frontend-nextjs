0f7e7a51efb85c683026f29c2b41ef5baa72e90f
Lajja123 <lajja.lamprostech@gmail.com>
2025-10-16 13:15:42 +0530

    Update SDK link ui

--- src/components/api/Documentation.tsx
+++ src/components/api/Documentation.tsx
@@ -19,22 +19,23 @@ function Documentation() {
         >
           Explore and integrate your project with TriggerX using API service.
         </Typography>
-        <Typography variant="h4" color="secondary" align="left" className="">
-          {" "}
-          Accelerate your development with our official SDKs — built for
-          seamless integration in your favorite language. Visit the SDK docs to
-          explore code samples, setup guides, and download links.
-          <br />
-          <a
-            href="https://www.npmjs.com/package/sdk-triggerx"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-500 hover:text-[#F8FF7C] underline"
-          >
-            Check out our SDK
-          </a>
-        </Typography>
-
+        <div className="flex flex-col gap-5 pb-3">
+          <Typography variant="h4" color="secondary" align="left" className="">
+            Accelerate your development with our official SDKs — built for
+            seamless integration in your favorite language. Visit the SDK docs
+            to explore code samples, setup guides, and download links.
+          </Typography>
+          <div>
+            <a
+              href="https://www.npmjs.com/package/sdk-triggerx"
+              target="_blank"
+              rel="noopener noreferrer"
+              className=" hover:bg-[#FFFFFF]  bg-[#F8FF7C] text-black px-4 py-3 rounded-full"
+            >
+              Check out our SDK
+            </a>
+          </div>
+        </div>
         <ApiDocsPage />
       </Card>
     </>
