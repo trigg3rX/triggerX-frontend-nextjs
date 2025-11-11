d93602ef6dda8967cd3243e0971c248b42842c98
darshitbhalodi5 <darshitbhalodi@gmail.com>
2025-11-11 15:30:25 +0530

    feat: Types and configuration for icon and all necessary params configuration to take input

---
+++ src/components/safe-wallet/helpers/buildIconItems.tsx
@@ -0,0 +1,171 @@
+import React from "react";
+import {
+  Timer,
+  SlidersHorizontalIcon,
+  Braces,
+  FunctionSquare,
+  ChevronUp,
+  ChevronDown,
+  AlarmClock,
+  CalendarCog,
+  Repeat2,
+  Link2,
+  ScrollTextIcon,
+  BellDot,
+  Ampersand,
+  SquareArrowOutUpRightIcon,
+  Cable,
+  RotateCcw,
+  RefreshCwOff,
+  FileCode,
+  Scroll,
+} from "lucide-react";
+import { BuildIconItemsArgs, IconSpec } from "@/types/icon-strip-parameter";
+
+export function buildIconItems(args: BuildIconItemsArgs): IconSpec[] {
+  const {
+    // Time based
+    scheduleType,
+    timeInterval,
+    cronExpression,
+    specificSchedule,
+    // Event based
+    triggerChainId,
+    triggerContractAddress,
+    triggerEvent,
+    // Condition based
+    conditionType,
+    upperLimit,
+    lowerLimit,
+    valueSourceType,
+    valueSourceUrl,
+    // Common
+    timeFrame,
+    dynamicArgumentsScriptUrl,
+    recurring,
+    abi,
+    // Contract based
+    targetContractAddress,
+    targetFunction,
+  } = args;
+
+  const items: IconSpec[] = [];
+
+  const add = (
+    condition: boolean,
+    key: string,
+    value: string | undefined,
+    icon: React.ReactNode,
+  ) => {
+    if (condition) {
+      items.push({ key, value, icon });
+    }
+  };
+
+  // Time based parameters
+  add(
+    !!scheduleType,
+    "scheduleType",
+    scheduleType,
+    <SlidersHorizontalIcon size={16} />,
+  );
+  add(
+    typeof timeInterval === "number",
+    "timeInterval",
+    typeof timeInterval === "number" ? String(timeInterval) : undefined,
+    <Repeat2 size={16} />,
+  );
+  add(
+    !!cronExpression,
+    "cronExpression",
+    cronExpression,
+    <CalendarCog size={16} />,
+  );
+  add(
+    !!specificSchedule,
+    "specificSchedule",
+    typeof specificSchedule === "number"
+      ? String(specificSchedule)
+      : specificSchedule,
+    <AlarmClock size={16} />,
+  );
+
+  // Event based parameters
+  add(
+    !!triggerChainId,
+    "triggerChainId",
+    typeof triggerChainId === "number" ? String(triggerChainId) : undefined,
+    <Link2 size={16} />,
+  );
+  add(
+    !!triggerContractAddress,
+    "triggerContractAddress",
+    triggerContractAddress,
+    <ScrollTextIcon size={16} />,
+  );
+  add(!!triggerEvent, "triggerEvent", triggerEvent, <BellDot size={16} />);
+
+  // Condition based parameters
+  add(!!conditionType, "conditionType", conditionType, <Ampersand size={16} />);
+  add(
+    typeof upperLimit === "number",
+    "upperLimit",
+    typeof upperLimit === "number" ? String(upperLimit) : undefined,
+    <ChevronUp size={16} />,
+  );
+  add(
+    typeof lowerLimit === "number",
+    "lowerLimit",
+    typeof lowerLimit === "number" ? String(lowerLimit) : undefined,
+    <ChevronDown size={16} />,
+  );
+  add(
+    !!valueSourceType,
+    "valueSourceType",
+    valueSourceType,
+    <Cable size={16} />,
+  );
+  add(
+    !!valueSourceUrl,
+    "valueSourceUrl",
+    valueSourceUrl,
+    <SquareArrowOutUpRightIcon size={16} />,
+  );
+
+  // Common parameters
+  add(
+    typeof timeFrame === "number",
+    "timeFrame",
+    typeof timeFrame === "number" ? String(timeFrame) : undefined,
+    <Timer size={16} />,
+  );
+  add(
+    !!dynamicArgumentsScriptUrl,
+    "dynamicArgumentsScriptUrl",
+    dynamicArgumentsScriptUrl ? "Script" : undefined,
+    <FileCode size={16} />,
+  );
+  add(
+    true,
+    "recurring",
+    recurring ? "Yes" : "No",
+    recurring ? <RotateCcw size={16} /> : <RefreshCwOff size={16} />,
+  );
+  add(!!abi, "abi", abi, <Braces size={16} />);
+
+  // Contract based parameters
+  add(
+    !!targetContractAddress,
+    "targetContractAddress",
+    targetContractAddress,
+    <Scroll size={16} />,
+  );
+  add(
+    !!targetFunction,
+    "targetFunction",
+    targetFunction,
+    <FunctionSquare size={16} />,
+  );
+
+  return items;
+}
